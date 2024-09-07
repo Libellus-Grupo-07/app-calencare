4854cf6c934f48f2cf3ee37daccd4cf10cf56c5b
jessicabarreirosm <jessica.martins@sptech.school>
2024-09-07 13:57:21 -0300

    tela de adicionar produto

--- app/src/main/java/school/sptech/TelaAdicionarProduto.kt
+++ app/src/main/java/school/sptech/TelaAdicionarProduto.kt
@@ -1,9 +1,9 @@
 package school.sptech
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -33,69 +34,106 @@ class TelaAdicionarProduto : ComponentActivity() {
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun TelaAdicionarProdutoScreen() {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Box(
-                        modifier = Modifier.fillMaxWidth(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            text = "Adicionar Produto",
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 20.sp,
-                            textAlign = TextAlign.Center
-                        )
-                    }
-                },
-                navigationIcon = {
-                    IconButton(onClick = { /* Ação do voltar */ }) {
-                        Image(
-                            painter = painterResource(id = R.mipmap.voltar),
-                            contentDescription = "Voltar",
-                            modifier = Modifier.size(30.dp)
-                        )
-                    }
-                }
-            )
+    Box(modifier = Modifier.fillMaxSize()) {
+        // Bolinha no canto superior direito
+        Canvas(modifier = Modifier
+            .size(100.dp)
+            .align(Alignment.TopEnd)
+            .offset(x = 50.dp, y = (-50).dp)) {
+            drawCircle(color = Color(0xFF9F36F0), radius = size.minDimension / 2, style = Fill)
         }
-    ) { paddingValues ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            Spacer(modifier = Modifier.height(32.dp))
 
-            ProductForm()
+        // Bolinha no canto inferior esquerdo
+        Canvas(modifier = Modifier
+            .size(100.dp)
+            .align(Alignment.BottomStart)
+            .offset(x = (-50).dp, y = 50.dp)) {
+            drawCircle(color = Color(0xFF9F36F0), radius = size.minDimension / 2, style = Fill)
+        }
 
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.End
-            ) {
-                OutlinedButton(
-                    onClick = { /* TODO: Handle cancel */ },
-                    colors = ButtonDefaults.outlinedButtonColors(
-                        containerColor = Color.Transparent, // Fundo transparente
-                        contentColor = Color.Gray // Cor do texto do botão
-                    ),
-                    modifier = Modifier.padding(end = 8.dp)
-                ) {
-                    Text("Cancelar")
-                }
-                Button(
-                    onClick = { /* TODO: Handle add */ },
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF9F36F0))
-                ) {
-                    Text("Adicionar", color = Color.White)
-                }
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Box(
+                            modifier = Modifier.fillMaxWidth(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = "Adicionar Produto",
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 20.sp,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = { /* Ação do voltar */ }) {
+                            Image(
+                                painter = painterResource(id = R.mipmap.voltar),
+                                contentDescription = "Voltar",
+                                modifier = Modifier.size(30.dp)
+                            )
+                        }
+                    }
+                )
             }
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Spacer(modifier = Modifier.height(32.dp))
+
+                ProductForm()
+
+                ButtonRow()
+            }
+        }
+    }
+}
+
+@Composable
+fun ButtonRow(
+    onCancelClick: () -> Unit = { /* TODO: Handle cancel */ },
+    onAddClick: () -> Unit = { /* TODO: Handle add */ }
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.End
+    ) {
+        OutlinedButton(
+            onClick = onCancelClick,
+            colors = ButtonDefaults.outlinedButtonColors(
+                containerColor = Color.Transparent,
+                contentColor = Color.Gray
+            ),
+            modifier = Modifier.padding(end = 8.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.mipmap.cancelar),
+                contentDescription = "Cancelar",
+                modifier = Modifier.size(18.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text("Cancelar")
+        }
+        Button(
+            onClick = onAddClick,
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF9F36F0))
+        ) {
+            Icon(
+                painter = painterResource(id = R.mipmap.adicionar),
+                contentDescription = "Adicionar",
+                modifier = Modifier.size(18.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp)) 
+            Text("Adicionar", color = Color.White)
         }
     }
 }
@@ -155,34 +193,23 @@ fun FormFieldWithLabel(
             fontSize = 14.sp,
             modifier = Modifier.padding(bottom = 4.dp)
         )
-        if (isMultiline) {
-            OutlinedTextField(
-                value = value,
-                onValueChange = onValueChange,
-                label = { Text(label) },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(124.dp)
-                    .clip(RoundedCornerShape(20.dp)), // Aumenta o arredondamento
-                shape = RoundedCornerShape(20.dp) // Aumenta o arredondamento
-            )
-        } else {
-            OutlinedTextField(
-                value = value,
-                onValueChange = onValueChange,
-                label = { Text(label) },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clip(RoundedCornerShape(20.dp)), // Aumenta o arredondamento
-                shape = RoundedCornerShape(20.dp) // Aumenta o arredondamento
-            )
-        }
+        OutlinedTextField(
+            value = value,
+            onValueChange = onValueChange,
+            label = { Text(label) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .let { if (isMultiline) it.height(124.dp) else it }
+                .clip(RoundedCornerShape(20.dp)),
+            shape = RoundedCornerShape(20.dp),
+            maxLines = if (isMultiline) Int.MAX_VALUE else 1
+        )
     }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview2() {
+fun TelaAdicionarProdutoPreview() {
     CalencareAppTheme {
         TelaAdicionarProdutoScreen()
     }
